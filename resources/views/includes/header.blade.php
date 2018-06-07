23bcc042e526a86157063098f13174a5bcb97992
simplon <dekorte.tsrit@gmail.com>
2018-06-07 14:54:57 +0200

    Ruud - adaptation headerheight and logos - in includes/header.blade.php and home_sr.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,13 +1,14 @@
-<header class="wrapper">
-	<div class="container">
-		<div class="row  text-center pt-4 ">
-			<div class="col-md-12 col-xs-12">
-				<img src="img/header_logo_simplon.png">
-			</div>
-			<div class="col-md-12 col-xs-12 pt-4 my-4">
-				<img src="img/header_logo_ern_and_ra.png">
-			</div>	
-		</div>	
-	</div>
+<header class="wrapper simplonheader">
+  <div class="container">
+    <div class="row  text-center pt-4 ">
+      <div class="col-md-12 col-xs-12">
+        <!-- Image stored in folder storage/app/public/img -->
+        <img src="img/header_logo_simplon.png" class="simplonlogo">
+      </div>
+      <div class="col-md-12 col-xs-12 pt-4 my-4">
+        <!-- Image stored in folder storage/app/public/img -->
+        <img src="img/header_logo_ern_and_ra.png" class="partnerlogos">
+      </div>  
+    </div>  
+  </div>
 </header>
-
