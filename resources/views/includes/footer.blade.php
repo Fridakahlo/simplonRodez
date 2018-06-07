e868d493c272507360d05d2a4f59c25712a71e2e
simplon <dekorte.tsrit@gmail.com>
2018-06-07 14:12:54 +0200

    Ruud - re-insert footer and row div's in footer.blade.php

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -123,34 +123,37 @@
 
 </style>
 
+<footer class="wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-sm-6">
+                <!-- Image stored in folder /public/img -->
+                <img src="img/footer_logo_ecole_regionale_numerique_224x100.png" class="img-fluid">
+            </div>
+            <div class="col-md-2 col-sm-6">
+                <!-- Image stored in folder /public/img -->
+                <img src="img/footer_logo_rodez_agglomeration_222x100.png" class="img-fluid">
+            </div>
+            <div class="col-md-1 col-sm-0">&nbsp;</div>
+            <div class="col-md-2 col-sm-12 text-center">
+                <!-- Image stored in folder /public/img -->
+                <img src="img/footer_logo_simplon_rodez_123x100.png" class="img-fluid">
+            </div>
 
-    <div class="col-md-2 col-sm-6">
-        <!-- Image stored in folder /public/img -->
-        <img src="img/footer_logo_ecole_regionale_numerique_224x100.png" class="img-fluid">
-    </div>
-    <div class="col-md-2 col-sm-6">
-        <!-- Image stored in folder /public/img -->
-        <img src="img/footer_logo_rodez_agglomeration_222x100.png" class="img-fluid">
-    </div>
-    <div class="col-md-1 col-sm-0">&nbsp;</div>
-    <div class="col-md-2 col-sm-12 text-center">
-        <!-- Image stored in folder /public/img -->
-        <img src="img/footer_logo_simplon_rodez_123x100.png" class="img-fluid">
-    </div>
+            <div class="col-md-5 col-sm-12 text-center social-box">
+                <ul class="social-network social-circle">
+                    <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f fa-2x"></i></a></li>
+                    <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter fa-2x"></i></a></li>
+                    <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fab fa-linkedin-in fa-2x"></i></a></li>
+                    <li><a href="#" class="icoGoogle" title="E-Mail"><i class="far fa-envelope fa-2x"></i></a></li>
+                </ul>               
+            </div>
 
-    <div class="col-md-5 col-sm-12 text-center social-box">
-        <ul class="social-network social-circle">
-            <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f fa-2x"></i></a></li>
-            <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter fa-2x"></i></a></li>
-            <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fab fa-linkedin-in fa-2x"></i></a></li>
-            <li><a href="#" class="icoGoogle" title="E-Mail"><i class="far fa-envelope fa-2x"></i></a></li>
-        </ul>               
-    </div>
-
-    <div class="col-md-12 col-sm-12">
-        <div class="footer-copyright text-center py-1 mt-2">&copy;&nbsp;2018 Copyright:
-            <a href="https://simplon.co/"> Simplon&nbsp;Rodez</a>
+            <div class="col-md-12 col-sm-12">
+                <div class="footer-copyright text-center py-1 mt-2">&copy;&nbsp;2018 Copyright:
+                    <a href="https://simplon.co/"> Simplon&nbsp;Rodez</a>
+                </div>
+            </div>
         </div>
     </div>
-
-     
+</footer>
