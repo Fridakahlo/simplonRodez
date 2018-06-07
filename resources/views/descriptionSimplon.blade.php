4c1786064906a73b0f02c9a360575d4e36c26cbc
david <david@david-ThinkPad-T420.com>
2018-06-07 21:09:08 +0200

    page simplon france restruturation du html CSS fini: david

--- resources/views/descriptionSimplon.blade.php
+++ resources/views/descriptionSimplon.blade.php
@@ -7,10 +7,10 @@
 	<div class="container-fluid">
 		<div class="row">
 		<!-- Block 1 -->
-			<div class="blockD block1">
+			<div class="block block1">
 				<div class="col-md-12">
 					<div class="col-md-6">
-						<h1>DÉVELOPPEUR.SE WEB</h1>
+						<h1>Développeur.se Web</h1>
 					</div>
 					<div class="d-flex">
 						<div class="col-md-6">					
@@ -110,12 +110,12 @@
 <div class="wrapper">
 	<div class="container-fluid">
 		<div class="row">		
-			<div class="blockD block2">
+			<div class="block block2">
 				<div class="col-md-12">
 					<div class="d-flex">
 						<div class="col-md-6">&nbsp;</div>
 						<div class="col-md-6">
-							<h1>LE DIPLÔME, LA DURÉE</h1>
+							<h1>Le Diplôme, la durée</h1>
 						</div>	
 					</div>	
 					<div class="d-flex">
@@ -141,17 +141,17 @@
 <div class="wrapper">
 	<div class="container-fluid">
 		<div class="row">	
-			<div class="blockD block3">
+			<div class="block block3">
 				<div class="col-md-12">	
 					<div class="col-md-6">
-						<h1>TÉMOIGNAGES</h1>
+						<h1>Témoignages :</h1>
 					</div>			
 					<div class="d-flex">
 						<div class="img col-md-6">
 							<img src="img/micro.jpg" class="imgDescription" alt="Responsive image">
 						</div>
 						<div class="col-md-6">
-							<h2>Témoignages :</h2>
+							<h2>Nom de l'apprenant :</h2>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus ipsam enim pv classspiciatis et autem consequuntur earum maxime laboriosam optio. Consequatur at dolore, culpa minus, fugiat iure necessitatib
 								<br>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla aspernatur, consequatur culpa, asperiores commodi impedit porro autem facere, sit ipsam provident id, minima expedita quidem ab aperiam tempore eos quis.	
