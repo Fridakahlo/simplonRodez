553288a94ecb54b443db90bd72a93f0f5a8c1cd1
fridakahlo <aj050630@gmail.com>
2018-06-07 15:15:47 +0200

    merges

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,13 +1,6 @@
 <header class="wrapper">
 	<div class="container">
-		<div class="row  text-center pt-4 ">
-			<div class="col-md-12 col-xs-12">
-				<img src="img/header_logo_simplon.png" height="35" width="47"">
-			</div>
-			<div class="col-md-12 col-xs-12 pt-4 my-4">
-				<img src="img/header_logo_ern_and_ra.png">
-			</div>	
-		</div>	
+			
 	</div>
 </header>
 
